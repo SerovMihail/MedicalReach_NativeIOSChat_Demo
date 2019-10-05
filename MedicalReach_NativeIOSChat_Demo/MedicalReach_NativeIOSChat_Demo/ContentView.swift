e69af8227691b5ab712b4830b70e25a39b60ef96
SerovMikhail <serov.mikhail.job@gmail.com>
2019-10-05 23:34:55 +0200

    init commit

---
+++ MedicalReach_NativeIOSChat_Demo/MedicalReach_NativeIOSChat_Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedicalReach_NativeIOSChat_Demo
+//
+//  Created by Mikhail on 05/10/2019.
+//  Copyright © 2019 Mikhail. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

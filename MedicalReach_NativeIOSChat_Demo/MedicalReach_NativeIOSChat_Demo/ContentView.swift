a2310693236845e6a4ea29d5aeb84ee7b0581454
SerovMikhail <serov.mikhail.job@gmail.com>
2019-10-06 14:19:04 +0200

    add asset to project

--- MedicalReach_NativeIOSChat_Demo/MedicalReach_NativeIOSChat_Demo/ContentView.swift
+++ MedicalReach_NativeIOSChat_Demo/MedicalReach_NativeIOSChat_Demo/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Hello")
+                .font(.largeTitle)
+                .font(.largeTitle)
+                .font(.largeTitle)
+                .font(.largeTitle)
+        }
     }
 }
 
